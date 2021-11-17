601a620b08bd3ccbfd463dd019289a40b75fe7cc
sssuleiman <92515718+sssuleiman@users.noreply.github.com>
2021-11-17 04:52:27 +0300

    Update and rename FLAge.java to name.java

--- name.java
+++ name.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
-  class FLAge{
+  class name{
       public static void main (String []args){
           Scanner scanner = new Scanner (System.in);
           System.out.println("Enter your first name");
           String firtName = scanner.nextLine();
 
           System.out.println("Enter your last name");
-           String lastName = scanner.nextLine();
+          String lastName = scanner.nextLine();
+          System.out.println ("what is ur age");
+          int age = scanner.nextInt();
+
+        
           System.out.println("Welcome " + firtName + " " + lastName + "!" );
       }  
       
