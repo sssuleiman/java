693527a073762b16e89e650a0c9ee3577179be38
Ramadhan Mohammed Mkoma <90829359+Ramadhanmkoma@users.noreply.github.com>
2021-11-17 04:37:31 -0500

    Word_game.java
    Whats new:- 
    -- printf statement
    -- animal_type variable

--- word_game.java
+++ word_game.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class word_game {
+class Word_game {
     public static void main (String [] args ) {
         Scanner scanner= new Scanner (System.in);
 
@@ -7,9 +7,10 @@ class word_game {
         String name = scanner.nextLine();
 
         System.out.println ("what is ur type of animal: ");
-        String petname = scanner.nextLine();
+        String animal_type = scanner.nextLine();
 
-        
+        System.out.println ("Enter Pet's name: );
+        String petname = scanner.next();
 
         System.out.println ("what is ur proffesional: ");
         String proffesional = scanner.nextLine();
@@ -22,8 +23,12 @@ class word_game {
 
         System.out.println ("what is ur age");
         int age = scanner.nextInt();
-
-        System.out.println ("there once was a person named" + name + "who lived in " + city + ".At the age of " + age + "," + name + "went to collage at "+ collage + "." + name + " graduated and went to work as " + proffesional + ". Then, " + name + "adopted a(n) Animal named" + petname + ". They both lived happily ever after!" );
+                            
+        /*
+            Adding printf to display an output.
+        */
+        System.out.printf("There once was a person named %s who lived in %s. At the age of %d, %s went to college at %s. %s graduated and went to work as %s. Then %s adopted a(n) %s named %s. They both lived happily ever after!", name, city, age, name, collage, name, professional, name, animal_type, petname);
+       // System.out.println ("\n\nthere once was a person named" + name + "who lived in " + city + ".At the age of " + age + "," + name + "went to collage at "+ collage + "." + name + " graduated and went to work as " + proffesional + ". Then, " + name + "adopted a(n) Animal named" + petname + ". They both lived happily ever after!" );
 
     }
 }
