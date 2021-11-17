6d93dbd40f04bd284d1ba294d416ce3e9ee4b0ee
sssuleiman <92515718+sssuleiman@users.noreply.github.com>
2021-11-17 05:26:37 +0300

    Update and rename MilesPerGallon.java to gallonpermile.java

--- gallonpermile.java
+++ gallonpermile.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class MilesPerGallon {
+public class gallonpermile {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of miles: ");
         double miles = input.nextDouble();
         System.out.println("Enter number of gallon of gas: ");
         double gallon = input.nextDouble();
-        double MPG = (miles / gallon);
-        System.out.println("Miles per gallon is: " + MPG);
+        double GPM = (miles / gallon);
+        System.out.println("Miles per gallon is: " + GPM);
     }
 }
