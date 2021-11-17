4ee499ea6e00367b557de02384c415b5ac506d5e
sssuleiman <92515718+sssuleiman@users.noreply.github.com>
2021-11-17 05:45:23 +0300

    Update gallonpermile.java

--- gallonpermile.java
+++ gallonpermile.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class gallonpermile {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         System.out.println("Enter number of miles: ");
         double miles = input.nextDouble();
+        
+        System.out.println("Enter price of gas per gallon: ");
+        double price = input.nextDouble();
+        
         System.out.println("Enter number of gallon of gas: ");
         double gallon = input.nextDouble();
-        double GPM = (miles / gallon);
-        System.out.println("Miles per gallon is: " + GPM);
+        
+        double GPM = (miles * gallon);
+        double POT = (250 * price);
+        System.out.println("The car can travel in Miles per gallon is: " + GPM +"milesppergallon");
+        System.out.println("The cost of travel in 250Miles is: " + POT );
     }
 }
